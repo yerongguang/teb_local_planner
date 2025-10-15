da57c91bb3916593755d9f4767f9efcf7031d947
fugesen <fugesen@orbbec.com>
2025-10-15 17:12:50 +0800

    Fix formatting and spacing in main.cpp for improved readability

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include "inc/robot_footprint_model.h"
 #include "inc/obstacles.h"
 #include "inc/optimal_planner.h"
-#include<boost/smart_ptr.hpp>
+#include <boost/smart_ptr.hpp>
 //
 #include <opencv2/opencv.hpp>
 
@@ -14,44 +14,44 @@ int main()
 {
     // 参数配置
     TebConfig config;
-    PoseSE2 start(-2,0,0);
-    PoseSE2 end(2,0,0);
+    PoseSE2 start(-2, 0, 0);
+    PoseSE2 end(2, 0, -0);
     std::vector<ObstaclePtr> obst_vector;
-    obst_vector.emplace_back(boost::make_shared<PointObstacle>(0,0));
+    obst_vector.emplace_back(boost::make_shared<PointObstacle>(0, 0));
     ViaPointContainer via_points;
     RobotFootprintModelPtr robot_model = boost::make_shared<CircularRobotFootprint>(0.4);
     auto visual = TebVisualizationPtr(new TebVisualization(config));
     auto planner = new TebOptimalPlanner(config, &obst_vector, robot_model, visual, &via_points);
-    cv::Mat show_map = cv::Mat::zeros(cv::Size(500,500),CV_8UC3);
+    cv::Mat show_map = cv::Mat::zeros(cv::Size(500, 500), CV_8UC3);
 
     int start_theta = 0;
     int end_theta = 0;
 
     // 创建滑动条，只调用一次
     cv::namedWindow("path");
-    cv::createTrackbar("start theta","path",&start_theta,100);
-    cv::createTrackbar("end theta","path",&end_theta,100);
+    cv::createTrackbar("start theta", "path", &start_theta, 100);
+    cv::createTrackbar("end theta", "path", &end_theta, 100);
 
     while (true)
     {
-        memset(show_map.data,0,500*500*3);
+        memset(show_map.data, 0, 500 * 500 * 3);
         try
         {
             start.theta() = start_theta * 0.1;
             end.theta() = end_theta * 0.1;
 
-            planner->plan(start,end);
+            planner->plan(start, end);
             std::vector<Eigen::Vector3f> path;
             planner->getFullTrajectory(path);
-            for(int i = 0;i < path.size() - 1;i ++)
+            for (int i = 0; i < path.size() - 1; i++)
             {
                 int x = (int)(path.at(i)[0] * 100.f + 250);
                 int y = (int)(path.at(i)[1] * 100.f + 250);
-                int next_x = (int)(path.at(i+1)[0] * 100.f + 250);
-                int next_y = (int)(path.at(i+1)[1] * 100.f + 250);
-                cv::line(show_map,cv::Point(x,y),cv::Point(next_x,next_y),cv::Scalar(255,255,255));
+                int next_x = (int)(path.at(i + 1)[0] * 100.f + 250);
+                int next_y = (int)(path.at(i + 1)[1] * 100.f + 250);
+                cv::line(show_map, cv::Point(x, y), cv::Point(next_x, next_y), cv::Scalar(255, 255, 255));
             }
-            cv::imshow("path",show_map);
+            cv::imshow("path", show_map);
         }
         catch (...)
         {
